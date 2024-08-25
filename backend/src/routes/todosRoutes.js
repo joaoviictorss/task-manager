85d5aabf4f0661a36787f2b86f6177971372dbbb
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-08-25 02:51:33 -0300

    initial commit

---
+++ backend/src/routes/todosRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const router = express.Router()
+
+// router.get('/todos', (req, res) => res.status(200).send('Router is working!'))
+
+module.exports = router
